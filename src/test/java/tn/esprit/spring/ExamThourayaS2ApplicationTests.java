676c53ba992a591e57709aada12ed0d181b8e500
styve-Kamga <47719494+Styvekamga@users.noreply.github.com>
2022-12-13 06:57:13 +0100

    mise a jour test

--- src/test/java/tn/esprit/spring/ExamThourayaS2ApplicationTests.java
+++ src/test/java/tn/esprit/spring/ExamThourayaS2ApplicationTests.java
@@ -14,7 +14,8 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import org.junit.jupiter.api.Test;
+import java.util.Date;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
